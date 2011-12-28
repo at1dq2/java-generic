9a965698f52dbe1ecffd2bd2b99dd9b763d4cf46
at1dq2 <fxysun@yahoo.com.cn>
2011-12-28 20:46:32 +0800

    6.5 The Principle of Truth in Advertising

---
+++ src/sub6and5/Wrong.java
@@ -0,0 +1,24 @@
+package sub6and5;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class Wrong {
+	public static <T> T[] toArray(Collection<T> c) {
+		T[] a = (T[]) new Object[c.size()]; // unchecked cast
+		int i = 0;
+		for (T x : c)
+			a[i++] = x;
+		return a;
+	}
+
+	public static void main(String[] args) {
+		List<String> strings = Arrays.asList("one", "two");
+		String[] a = toArray(strings); // class cast error
+		/*
+		String[] a = (String[])toArray(strings);
+		*/ 
+	}
+
+}
